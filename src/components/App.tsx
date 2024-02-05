ae81b43e48b74976d28b5674a81669d69cb0b505
Florentin Portets <florentin.portets@ynov.com>
2024-02-05 10:13:18 +0100

    Préparation Grid

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
 import '../styles/App.css'
+import Grid from '../components/Grid'
 
 export default function App() {
 
   return (
     <>
+      <Grid/>
     </>
   )
 }
