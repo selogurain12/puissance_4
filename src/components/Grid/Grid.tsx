3b3ad49688000f08c49745712edb6ea84254028e
eliot guihal <eliot.guihal@ynov.com>
2024-02-12 09:15:54 +0000

    Fix grid

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -6,31 +6,39 @@ interface Props {
   rows: number;
   cols: number;
 }
-const Grid:React.FC<Props> = ({ rows, cols }) => {
-  const arrRows = Array.from({ length: rows }, (_, index) => index + 1);
-  const arrCols = Array.from({ length: cols }, (_, index) => index + 1);
-  const [currentPlayer, setCurrentPlayer] = useState('red');
-  const [currentCase, setCurrentCase] = useState('');
+
+const Grid: React.FC<Props> = ({ rows, cols }) => {
+  const [currentPlayer, setCurrentPlayer] = useState<'red' | 'yellow'>('red');
+  const initialGrid = Array.from({ length: rows }, () => Array(cols).fill(null));
+  const [grid, setGrid] = useState<string[][]>(initialGrid);
   const { gridContainer, rowContainer, colContainer } = styles;
-  const handleClick = () => {
-    setCurrentPlayer(currentPlayer === 'red' ? 'yellow' : 'red');
+
+  const handleClick = (row: number, col: number) => {
+    if (!grid[row][col]) {
+      const newGrid = [...grid];
+      newGrid[row][col] = currentPlayer;
+      setGrid(newGrid);
+      setCurrentPlayer(currentPlayer === 'red' ? 'yellow' : 'red');
+    }
   };
-  const Square = () => (
-    <Box sx={colContainer} onClick={() => handleClick()}>
-      {currentCase === '' && currentPlayer && <img src={`../src/assets/${currentPlayer}.svg`} alt={currentPlayer} />}
+
+  const Square: React.FC<{ value: string | null; row: number; col: number }> = ({ value, row, col }) => (
+    <Box sx={colContainer} onClick={() => handleClick(row, col)}>
+      {value && <img src={`../src/assets/${value}.svg`} alt={value} />}
     </Box>
   );
+
   return (
     <Box sx={gridContainer}>
-      {arrRows.map((row) => (
-        <Stack key={row} sx={rowContainer} direction="row">
-          {arrCols.map((col) => (
-            <Square key={col} />
-
+      {grid.map((row: string[], rowIndex: number) => (
+        <Stack key={rowIndex} sx={rowContainer} direction="row">
+          {row.map((col: string, colIndex: number) => (
+            <Square key={colIndex} value={col} row={rowIndex} col={colIndex} />
           ))}
         </Stack>
       ))}
     </Box>
   );
 };
+
 export default Grid;
