d810874615b8922f75118018219605075f09eadd
Florentin Portets <florentin.portets@ynov.com>
2024-02-12 11:53:44 +0100

    Condition de Victoire

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -23,8 +23,58 @@ const Grid: React.FC = () => {
         return row;
       }
     }
-    return -1; // Column is full
+    return -1;
   };
+
+  const checkForWinner = (row: number, col: number): boolean => {
+    const player = grid[row][col];
+
+    for (let row = 0; row < numRows; row++) {
+      for (let col = 0; col <= numCols - 4; col++) {
+          if (grid[row][col] === player &&
+              grid[row][col + 1] === player &&
+              grid[row][col + 2] === player &&
+              grid[row][col + 3] === player) {
+              return true;
+          }
+      }
+  }
+
+  for (let col = 0; col < numCols; col++) {
+      for (let row = 0; row <= numRows - 4; row++) {
+          if (grid[row][col] === player &&
+              grid[row + 1][col] === player &&
+              grid[row + 2][col] === player &&
+              grid[row + 3][col] === player) {
+              return true;
+          }
+      }
+  }
+
+  for (let row = 0; row <= numRows - 4; row++) {
+      for (let col = 0; col <= numCols - 4; col++) {
+          if (grid[row][col] === player &&
+              grid[row + 1][col + 1] === player &&
+              grid[row + 2][col + 2] === player &&
+              grid[row + 3][col + 3] === player) {
+              return true;
+          }
+      }
+  }
+
+  for (let row = 3; row < numRows; row++) {
+      for (let col = 0; col <= numCols - 4; col++) {
+          if (grid[row][col] === player &&
+              grid[row - 1][col + 1] === player &&
+              grid[row - 2][col + 2] === player &&
+              grid[row - 3][col + 3] === player) {
+              return true;
+          }
+      }
+  }
+
+  return false;
+  }
 
   const handleClick = (col: number): void => {
     const row = findValidRow(col);
@@ -34,13 +84,17 @@ const Grid: React.FC = () => {
       newGrid[row][col] = currentPlayer;
       setGrid(newGrid);
 
-      // Add logic to check for a winner
+      if (checkForWinner(row, col)) {
+        console.log(`${currentPlayer} wins!`);
+      } else {
+        setCurrentPlayer(currentPlayer === 'red' ? 'yellow' : 'red');
+      }
 
       setCurrentPlayer(currentPlayer === 'red' ? 'yellow' : 'red');
     }
   };
 
-  const Square: React.FC<SquareProps> = ({ value, col, row }) => {
+  const Square: React.FC<SquareProps> = ({ value, col}) => {
     return (
       <Box sx={colContainer} onClick={() => handleClick(col)}>
         {value && <img src={`../src/assets/${value}.svg`} alt={value} />}
