0447bce0dee971976be05d9847df2fa24eb0c2f1
selogurain12 <lora80123@gmail.com>
2024-02-12 10:03:42 +0100

    feat(puissance_4/api): create server for websocket

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
-// server/index.js
 const express = require('express');
 const http = require('http');
 const { Server } = require("socket.io");
+const moment = require('moment'); // Assurez-vous d'avoir moment.js installé
 
 const app = express();
 const server = http.createServer(app);
@@ -10,19 +10,40 @@ const io = require('socket.io')(server, {
       origin: "*",
       methods: ["GET", "POST"],
     }
-  });
-  
+});
+
+// Initialisation de l'objet users pour suivre les utilisateurs connectés
+let users = {};
 
 io.on('connection', (socket) => {
   console.log('Un utilisateur est connecté');
 
-  socket.on('disconnect', () => {
-    console.log('Utilisateur déconnecté');
+  // Vous pouvez ajouter ici une gestion pour stocker l'utilisateur lors de la connexion
+  socket.on('username', (username) => {
+    users[socket.id] = { id: socket.id, name: username };
+    // Informer tous les clients de la liste mise à jour des utilisateurs
+    io.emit('users', Object.values(users));
   });
 
   socket.on('message', (msg) => {
     console.log('Message reçu:', msg);
     io.emit('message', msg);
+  });
+
+  socket.on('send', message => {
+    // Assurez-vous que users[socket.id] existe avant de l'utiliser
+    const user = users[socket.id];
+    if (user) {
+      io.emit('message', { user: user, text: message, date: moment().toISOString() });
+    }
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected: ' + socket.id);
+    // Supprimer l'utilisateur de l'objet users
+    delete users[socket.id];
+    // Informer tous les clients de la liste mise à jour des utilisateurs
+    io.emit('users', Object.values(users));
   });
 });
 
